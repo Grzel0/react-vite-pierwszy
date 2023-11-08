438f7dfc8c31c8a562f0ade49940700a43fd2393
grzegorz <grzegorz.gejlo@cyfroweszkoly.pl>
2023-11-08 16:05:28 +0100

    lista zadan(cos nie dziala)

---
+++ todoapp/src/ToDo.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react"
+
+const tasks = [
+    {id:1, text:"zadanie 1"},
+    {id:2, text:"zadanie 2"},
+    {id:3, text:"zadanie 3"},
+    {id:4, text:"zadanie 4"},
+    {id:5, text:"zadanie 5"},
+]
+
+
+const ToDO = ()=>{
+    const [todos, setToDos] = useState(tasks)
+
+    const removeTaskHandler = (taskId) =>{
+        const newTasks = todos.filter( (t) => t.id !== taskId)
+        setToDos(newTasks)
+    }
+    return(
+        <>
+        <h1>Jestem z komponentu ToDo</h1>
+        <ul>
+            {
+                tasks.map( t =>(
+                    <li style={ {color:"red", fontSize:"20px", marginBottom:"10px", backgroundColor:"blue"} }key={t.id}>
+                        <span>
+                            {t.text}
+                        </span>
+                        <button onClick={ () => removeTaskHandler(t.id)}>usuń zadanie</button>
+                    </li>
+                )) 
+            }
+        </ul>
+        </>
+    )
+    
+}
+
+export default ToDO
